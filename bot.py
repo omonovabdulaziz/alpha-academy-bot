b47b8f34e3e02c9732970189c42c79a5ac198bb2
golanguzb71 <abdulaziomonov4@gmail.com>
2024-09-24 14:41:21 +0500

    inline_keyboard_sertificate_ste_up_added

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from config.config import IS_MIGRATE
 from database.postgres import create_tables
-from helpers.helpers import handle_contact_helper, bot, send_welcome_helper
+from helpers.helpers import handle_contact_helper, bot, send_welcome_helper, handle_code
 import asyncio
 
 
@@ -12,6 +12,11 @@ async def handle_contact(message):
 @bot.message_handler(commands=['start'])
 async def send_welcome(message):
     await send_welcome_helper(message)
+
+
+@bot.message_handler(content_types=['text'])
+async def handle_any_message(message):
+    await handle_code(message)
 
 
 if __name__ == '__main__':
--- helpers/extraHelpers.py
+++ helpers/extraHelpers.py
@@ -1,20 +1,25 @@
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-from telethon import TelegramClient, errors
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from telethon import TelegramClient
+
+from database.postgres import get_db_connection
 
 
-async def send_message(message, bot, messageField):
+async def send_message(message, bot, messageField, state):
     markup = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     contact_button = KeyboardButton("📞 Send Contact", request_contact=True)
     markup.add(contact_button)
-    await bot.send_message(message.chat.id, messageField, reply_markup=markup)
+    if state:
+        await bot.send_message(message.chat.id, messageField, reply_markup=markup)
+    else:
+        await bot.send_message(message.chat.id, messageField, parse_mode="HTML")
 
 
 async def send_welcome_admin(message, bot):
     name = message.from_user.first_name
     welcome_message = f"""
-Salom {name}. Siz tizimda admin rolidasiz.
+    Salom {name}. Siz tizimda <code> admin </code> rolidasiz , kerakli amalni tanlang.
     """
-    await bot.send_message(message.chat.id, welcome_message)
+    await send_message(message, bot, welcome_message, False)
 
 
 api_id = '20182242'
@@ -36,3 +41,82 @@ async def check_exist_in_required_channel(chat_id, channels):
                 membership_status.append(False)
 
         return all(membership_status)
+
+
+def save_contact_to_db(chat_id, phone_number, first_name, last_name):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    try:
+        cur.execute("""
+            INSERT INTO users (chat_id, phone_number, first_name, last_name)
+            VALUES (%s, %s, %s, %s)
+            ON CONFLICT (chat_id) DO UPDATE SET
+                phone_number = EXCLUDED.phone_number,
+                first_name = EXCLUDED.first_name,
+                last_name = EXCLUDED.last_name;
+        """, (chat_id, phone_number, first_name, last_name))
+
+        conn.commit()
+    except Exception as e:
+        print(f"Error saving contact to database: {e}")
+    finally:
+        cur.close()
+        conn.close()
+
+
+def check_user_exist_phone_number(phone_number):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT COUNT(*) FROM users WHERE phone_number = %s", (phone_number,))
+        count = cur.fetchone()[0]
+        return count > 0
+    except Exception as e:
+        print(f"Error checking phone number: {e}")
+        return False
+    finally:
+        cur.close()
+        conn.close()
+
+
+def check_user_exist_by_chat_id(chat_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT COUNT(*) FROM users WHERE chat_id = %s", (chat_id,))
+        count = cur.fetchone()[0]
+        return count > 0
+    except Exception as e:
+        print(f"Error checking chat_id : {e}")
+        return False
+    finally:
+        cur.close()
+        conn.close()
+
+
+def get_result_by_code(code):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT code, math_ball, english_ball, common_ball FROM results WHERE code = %s", (code,))
+        result = cur.fetchone()
+        if result:
+            message_text = f"""
+<code>1. Sizning codingiz: </code> {result[0]}
+<code>2. Matematika balingiz: </code> {result[1]}
+<code>3. Ingliz tili balingiz: </code> {result[2]}
+<code>4. Umumiy balingiz: </code> {result[3]}
+            """
+            markup = InlineKeyboardMarkup()
+            download_button = InlineKeyboardButton("📄 Sertifikatni yuklab olish", callback_data='download_certificate')
+            markup.add(download_button)
+
+            return message_text, markup
+        else:
+            return f"No result found for code: {code}", None
+    except Exception as e:
+        return f"Error while getting code: {str(e)}", None
+    finally:
+        cur.close()
+        conn.close()
