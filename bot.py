3d481e08221679c0fbb5c38476cdf8e8dde654c0
golanguzb71 <abdulaziomonov4@gmail.com>
2024-09-25 07:16:02 +0500

    only_1_channel_remained

--- bot.py
+++ bot.py
@@ -58,7 +58,7 @@ async def handle_any_message(message):
     if str(chat_id) == ADMIN_CHAT_ID:
         await handle_result(message)
     else:
-        check = await check_exist_in_required_channel(chat_id, ["abdulazizomonovblog", "sh0kh_07"])
+        check = await check_exist_in_required_channel(chat_id, ["IT_LIVE_GULISTON"])
         if not check:
             await bot.send_message(chat_id, "Iltimos talab qilingan kanallarga azo bo'ling !!")
             return
